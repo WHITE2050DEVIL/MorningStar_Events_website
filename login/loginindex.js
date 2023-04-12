c15cbab1bb3a44079c8dfae1192a8ad9a9e951bc
sairaj satarkar <satarkarsairaj3717@gmail.com>
2023-04-13 03:13:13 +0530

    update

--- login/loginindex.js
+++ login/loginindex.js
@@ -1,12 +1,12 @@
 // Fetch API for login
-const loginUser = async (eml, pwd) => {
+const loginUser = async (email, password) => {
     try {
       const response = await fetch('http://localhost:3005/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ eml, pwd })
+        body: JSON.stringify({ email, password })
       });
   
       if (response.ok) {
@@ -27,30 +27,34 @@ const loginUser = async (eml, pwd) => {
   };
   
   // Fetch API for signup
-  const signupUser = async (username, password, email) => {
-    try {
-      const response = await fetch('https://example.com/api/signup', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username, password, email })
-      });
   
-      if (response.ok) {
-        const data = await response.json();
-        // Process the data returned from the API
-        console.log('Signup successful:', data);
-        // Perform additional actions, such as updating UI or setting authentication tokens
-      } else {
-        // Handle error response from the API
-        console.error('Signup failed:', response.statusText);
-        // Perform error handling, such as showing an error message
-      }
-    } catch (error) {
-      // Handle any other errors, such as network issues
-      console.error('An error occurred:', error);
+// Function to handle sign up
+const signupUser = async () => {
+  // Get form input values
+  const userName = document.getElementById('UserName').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  try {
+    const response = await fetch('http://localhost:3005/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ userName, email, password }) // Use the collected form input values
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log('Signup successful:', data);
+      // Perform additional actions, such as updating UI or setting authentication tokens
+    } else {
+      console.error('Signup failed:', response.statusText);
       // Perform error handling, such as showing an error message
     }
-  };
+  } catch (error) {
+    console.error('An error occurred:', error);
+    // Perform error handling, such as showing an error message
+  }
+};
   
